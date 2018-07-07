9eb4644effa99f255a5c1892b4922ae6af9c42d6
JAN151718 <40213316+JAN151718@users.noreply.github.com>
2018-07-07 17:50:30 -0600

    Create Identification_VG.java

---
+++ Protection/Identification_VG.java
@@ -0,0 +1,13 @@
+package com.example.jagma.login_screen;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Vulnerable_Group_Identification extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vulnerable__group__identification);
+    }
+}
