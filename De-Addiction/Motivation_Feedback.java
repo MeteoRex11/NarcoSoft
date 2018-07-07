ae3c5ae22864fc32c883fb066b787149219f5d6b
JAN151718 <40213316+JAN151718@users.noreply.github.com>
2018-07-07 16:49:37 -0600

    Create Motivation_Feedback.java

---
+++ De-Addiction/Motivation_Feedback.java
@@ -0,0 +1,38 @@
+package com.example.jagma.login_screen;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Motivation_feedback extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_motivation_feedback);
+    }
+
+    public void Proceed(View view){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Are you sure you want to submit?")
+                .setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        Intent i=new  Intent(Motivation_feedback.this,Submit_screen.class);
+                        startActivityForResult(i,2);
+                        finish();
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+        AlertDialog alert = builder.create();
+        alert.show();
+
+    }
+}
