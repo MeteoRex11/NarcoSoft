7b07c4bd557fd471d84b6752d0100796da9c209b
JAN151718 <40213316+JAN151718@users.noreply.github.com>
2018-07-07 23:42:51 -0600

    Update Identification_VG.java

--- Protection/Identification_VG.java
+++ Protection/Identification_VG.java
@@ -1,7 +1,11 @@
 package com.example.jagma.login_screen;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Vulnerable_Group_Identification extends AppCompatActivity {
 
@@ -9,5 +13,26 @@ public class Vulnerable_Group_Identification extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vulnerable__group__identification);
+    }
+
+    public void Proceed(View view){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Are you sure you want to submit?")
+                .setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        Intent i=new  Intent(Vulnerable_Group_Identification.this,Submit_screen.class);
+                        startActivityForResult(i,2);
+                        finish();
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+        AlertDialog alert = builder.create();
+        alert.show();
+
     }
 }
