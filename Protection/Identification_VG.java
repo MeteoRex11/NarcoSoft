01ffc344a2f8e648ce7db75e24e9ba137c2f0a8a
JAN151718 <40213316+JAN151718@users.noreply.github.com>
2018-07-12 18:16:02 -0500

    Update Identification_VG.java

--- Protection/Identification_VG.java
+++ Protection/Identification_VG.java
@@ -6,13 +6,21 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class Vulnerable_Group_Identification extends AppCompatActivity {
+    
+    EditText p_i_a1, p_i_a2, p_i_a3, p_i_a4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vulnerable__group__identification);
+        
+        p_i_a1 = findViewById(R.id.p_i_a1);
+        p_i_a2 = findViewById(R.id.p_i_a2);
+        p_i_a3 = findViewById(R.id.p_i_a3);
+        p_i_a4 = findViewById(R.id.p_i_a4);
     }
 
     public void Proceed(View view){
@@ -24,6 +32,16 @@ public class Vulnerable_Group_Identification extends AppCompatActivity {
                         Intent i=new  Intent(Vulnerable_Group_Identification.this,Submit_screen.class);
                         startActivityForResult(i,2);
                         finish();
+
+                        String VulnerableGroups_Identified = p_i_a1.getText().toString();
+                        String VulnerableGroups_Counseled =p_i_a2.getText().toString();
+                        String Problems = p_i_a3.getText().toString();
+                        String Suggestions =  p_i_a4.getText().toString();
+                        
+
+                        String type="vg identification";
+                        BackgroundWorker backgroundWorker = new BackgroundWorker(Vulnerable_Group_Identification.this);
+                        backgroundWorker.execute(type, VulnerableGroups_Identified, VulnerableGroups_Counseled, Problems, Suggestions );
                     }
                 })
                 .setNegativeButton("No", new DialogInterface.OnClickListener() {
