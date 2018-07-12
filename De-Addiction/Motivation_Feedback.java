23affaee67cfa90dc8be1336a354cdad2e1a4100
JAN151718 <40213316+JAN151718@users.noreply.github.com>
2018-07-12 17:08:27 -0500

    Update Motivation_Feedback.java

--- De-Addiction/Motivation_Feedback.java
+++ De-Addiction/Motivation_Feedback.java
@@ -6,13 +6,33 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class Motivation_feedback extends AppCompatActivity {
+
+    EditText d_m_a1, d_m_a3, d_m_a4, d_m_a5, d_m_a6, d_m_a7, d_m_a8;
+    RadioButton radiobt;
+    RadioGroup ApproachMethod;
+    String d_m_radioText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_motivation_feedback);
+
+        int rbt = ApproachMethod.getCheckedRadioButtonId();
+        radiobt = findViewById(rbt);
+        d_m_radioText = radiobt.getText().toString();
+        
+        d_m_a1 = findViewById(R.id.d_m_a1);
+        d_m_a3 = findViewById(R.id.d_m_a3);
+        d_m_a4 = findViewById(R.id.d_m_a4);
+        d_m_a5 = findViewById(R.id.d_m_a5);
+        d_m_a6 = findViewById(R.id.d_m_a6);
+        d_m_a7 = findViewById(R.id.d_m_a7);
+        d_m_a8 = findViewById(R.id.d_m_a8);
     }
 
     public void Proceed(View view){
@@ -24,6 +44,20 @@ public class Motivation_feedback extends AppCompatActivity {
                         Intent i=new  Intent(Motivation_feedback.this,Submit_screen.class);
                         startActivityForResult(i,2);
                         finish();
+
+                        String Approach_Place = d_m_a1.getText().toString();
+                        String Approach_Method = d_m_radioText;;
+                        String Victim_Approach_Problems = d_m_a3.getText().toString();;
+                        String Family_Approach_Problems =  d_m_a4.getText().toString();;
+                        String Arguments = d_m_a5.getText().toString();;
+                        String Convincing_Factor = d_m_a6.getText().toString();;
+                        String Victim_Attitude = d_m_a7.getText().toString();;
+                        String Suggestions =  d_m_a8.getText().toString();;
+
+                        String type="deaddiction motivation";
+                        BackgroundWorker backgroundWorker = new BackgroundWorker(Motivation_feedback.this);
+                        backgroundWorker.execute(type, Approach_Place, Approach_Method, Victim_Approach_Problems, Family_Approach_Problems,
+                                Arguments, Convincing_Factor, Victim_Attitude, Suggestions );
                     }
                 })
                 .setNegativeButton("No", new DialogInterface.OnClickListener() {
