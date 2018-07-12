87e613ad62a15f11e4207007cc72b41080000509
JAN151718 <40213316+JAN151718@users.noreply.github.com>
2018-07-12 05:01:09 -0500

    update_Awareness_activity

--- Awareness/Awareness_Activity.java
+++ Awareness/Awareness_Activity.java
@@ -6,13 +6,30 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
 
 public class Awareness_Activity extends AppCompatActivity {
+
+    EditText no_of_peopleet,feedbacket,probet,suggestionet;
+    RadioGroup radioGroup1,radioGroup2;
+    RadioButton radioButton01,radioButton02;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_awareness_);
+
+        no_of_peopleet=findViewById(R.id.a_a3);
+        feedbacket=findViewById(R.id.a_a4);
+        probet=findViewById(R.id.a_a5);
+        suggestionet=findViewById(R.id.a_a6);
+        radioGroup1=findViewById(R.id.RGrp1);
+        radioGroup2=findViewById(R.id.RGrp2);
+
+
     }
 
     public void Proceed(View view){
@@ -21,9 +38,23 @@ public class Awareness_Activity extends AppCompatActivity {
                 .setCancelable(false)
                 .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
-                        Intent i=new  Intent(Awareness_Activity.this,Submit_screen.class);
-                        startActivityForResult(i,4);
-                        finish();
+                        int select_attemptID=radioGroup1.getCheckedRadioButtonId();
+                        int select_activityID=radioGroup2.getCheckedRadioButtonId();
+                        radioButton01=findViewById(select_attemptID);
+                        radioButton02=findViewById(select_activityID);
+                        String str_attempt_status=radioButton01.getText().toString();
+                        String str_no_of_people=no_of_peopleet.getText().toString();
+
+                        String str_probs=probet.getText().toString();
+                        String str_activity=radioButton02.getText().toString();
+                        String str_feedback=feedbacket.getText().toString();
+                        String str_suggestion=suggestionet.getText().toString();
+
+                        String type="submit";
+                        BackgroundWorker backgroundWorker=new BackgroundWorker(Awareness_Activity.this);
+                        backgroundWorker.execute(type,str_attempt_status,str_activity,str_no_of_people,str_feedback,str_probs,str_suggestion);
+
+
                     }
                 })
                 .setNegativeButton("No", new DialogInterface.OnClickListener() {
