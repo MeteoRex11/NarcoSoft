cee0cb5d3a6372ebb11762279986251109102968
JAN151718 <40213316+JAN151718@users.noreply.github.com>
2018-07-17 18:14:42 -0500

    Update login.php

--- PHP files/login.php
+++ PHP files/login.php
@@ -5,7 +5,7 @@ $date=$_POST["date"];
 $mysql_qry = "select * from login_screen where UID_number like '$UID' and birth_date like '$date';";
 $result = mysqli_query($con ,$mysql_qry);
 if(mysqli_num_rows($result)>0){
-	echo "Login sucessfull";
+	echo "Login Sucessfull";
 	
 }
 else{
