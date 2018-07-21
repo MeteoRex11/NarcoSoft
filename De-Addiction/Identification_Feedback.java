963a9b3f46608ca4299e03ee4e2ae28766f14f39
JAN151718 <40213316+JAN151718@users.noreply.github.com>
2018-07-21 11:46:58 +0530

    Update Identification_Feedback.java

--- De-Addiction/Identification_Feedback.java
+++ De-Addiction/Identification_Feedback.java
@@ -7,10 +7,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.preference.PreferenceManager;
+import android.content.SharedPreferences;
 
 public class Identification_Feedback extends AppCompatActivity {
 
-    EditText d_i_a1, d_i_a2, d_i_a3, d_i_a4, d_i_a5, d_i_a6, d_i_a7, d_i_a8, d_i_a9, d_i_a10, d_i_a11;
+    EditText d_i_a1, d_i_a2, d_i_a3, d_i_a4, d_i_a5, d_i_a6, d_i_a7, d_i_a8, d_i_a9, d_i_a10, d_i_a11, d_i_a12, d_i_details1, d_i_details2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +30,9 @@ public class Identification_Feedback extends AppCompatActivity {
         d_i_a9 =  findViewById(R.id.d_i_a9);
         d_i_a10 =  findViewById(R.id.d_i_a10);
         d_i_a11 =  findViewById(R.id.d_i_a11);
+        d_i_a12 =  findViewById(R.id.d_i_a12);
+        d_i_details1 = findViewById(R.id.d_i_details1);
+        d_i_details2 = findViewById(R.id.d_i_details2);
     }
 
     public void Proceed(View view){
@@ -36,28 +41,34 @@ public class Identification_Feedback extends AppCompatActivity {
                 .setCancelable(false)
                 .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
-                        Intent i=new  Intent(Identification_Feedback.this,Submit_screen.class);
-                        startActivityForResult(i,2);
-                        finish();
 
                         String Users = d_i_a1.getText().toString();
-                        String Narcotics_Type = d_i_a2.getText().toString();;
-                        String Narcotics_Quantity = d_i_a3.getText().toString();;
-                        String Narcotics_Price =  d_i_a4.getText().toString();;
-                        String Drug_Abuse_Reason = d_i_a5.getText().toString();;
-                        String Drug_Abuse_Hotspot = d_i_a6.getText().toString();;
-                        String Drug_Abusers_Gathering_Areas = d_i_a7.getText().toString();;
-                        String Supply_Time =  d_i_a8.getText().toString();;
-                        String Abuse_Time = d_i_a9.getText().toString();;
-                        String Place_Of_Purchase = d_i_a10.getText().toString();;
-                        String Suggestions = d_i_a11.getText().toString();;
+                        String User_Name = d_i_details1.getText().toString();
+                        String User_Address = d_i_a2.getText().toString();
+                        String Narcotics_Quantity = d_i_a3.getText().toString();
+                        String Narcotics_Price =  d_i_a4.getText().toString();
+                        String Drug_Abuse_Reason = d_i_a5.getText().toString();
+                        String Drug_Abuse_Hotspot = d_i_a6.getText().toString();
+                        String Drug_Abusers_Gathering_Areas = d_i_a7.getText().toString();
+                        String Supply_Time =  d_i_a8.getText().toString();
+                        String Supplier_Name = d_i_details2.getText().toString();
+                        String Supplier_Address = d_i_a9.getText().toString();
+                        String Abuse_Time =  d_i_a10.getText().toString();
+                        String Place_Of_Purchase = d_i_a11.getText().toString();
+                        String Suggestions = d_i_a12.getText().toString();
 
                         String type="deaddiction identification";
                         BackgroundWorker backgroundWorker = new BackgroundWorker(Identification_Feedback.this);
-                        backgroundWorker.execute(type, Users, Narcotics_Type, Narcotics_Quantity, Narcotics_Price,
+
+                        SharedPreferences sharedPreferences= PreferenceManager.getDefaultSharedPreferences(Identification_Feedback.this);
+                        String ID =sharedPreferences.getString("ID","unknown");
+
+                        backgroundWorker.execute(type, Users, User_Name, User_Address, Narcotics_Quantity, Narcotics_Price,
                                 Drug_Abuse_Reason, Drug_Abuse_Hotspot, Drug_Abusers_Gathering_Areas, Supply_Time,
-                                Abuse_Time, Place_Of_Purchase, Suggestions );
-                       
+                                Supplier_Name, Supplier_Address, Abuse_Time, Place_Of_Purchase, Suggestions, ID );
+
+
+
                     }
                 })
                 .setNegativeButton("No", new DialogInterface.OnClickListener() {
