c14cca3fe396bb38ba905f5348928336961bba7f
JAN151718 <40213316+JAN151718@users.noreply.github.com>
2018-08-05 18:28:00 +0530

    Update deaddiction_identification.php

--- PHP files/deaddiction_identification.php
+++ PHP files/deaddiction_identification.php
@@ -1,20 +1,20 @@
 <?php
 require "Bridge.php";
 
-$Number_of_Narcotics_Users=$_POST["Num_Narc_Usr"];
-$Type_of_Drug_Used=$_POST["Typ_Drg"];
-$Quantity_of_Drugs_Used=$_POST["Quan_Drg"];
-$Price_of_Drugs=$_POST["Prc_Drg"];
-$Reason_for_Drug_Abuse=$_POST["Rsn_Drg"];
-$Drug_Abuse_Prone_Area=$_POST["Drug_Prn_Area"];
-$Gathering_Area_for_Abuser=$_POST["Gathering_Area"];
-$Time_of_Supply=$_POST["Supply_Time"];
-$Time_of_Consumption=$_POST["Consump_Time"];
-$Place_of_Purchase=$_POST["Plc_Prchse"];
-$Suggestions=$_POST["Sggs"];
+$Number_of_Narcotics_Users=$_POST["num_narc_usr"];
+$Type_of_Drug_Used=$_POST["typ_drg"];
+$Quantity_of_Drugs_Used=$_POST["quan_drg"];
+$Price_of_Drugs=$_POST["prc_drg"];
+$Reason_for_Drug_Abuse=$_POST["rsn_drg"];
+$Drug_Abuse_Prone_Area=$_POST["drug_arn_area"];
+$Gathering_Area_for_Abuser=$_POST["gathering_area"];
+$Time_of_Supply=$_POST["supply_time"];
+$Time_of_Consumption=$_POST["consump_time"];
+$Place_of_Purchase=$_POST["plc_prchse"];
+$Suggestions=$_POST["sggs_id"];
 
 
-$mysql_qry = "insert into awareness_info(Num_Narc_Usr,Typ_Drg,Quan_Drg,Prc_Drg,Rsn_Drg, Drug_Prn_Area,Gathering_Area,Supply_Time,Consump_Time,Plc_Prchse,Sggs) 
+$mysql_qry = "insert into awareness_info(num_narc_usr,typ_drg,quan_drg,prc_drg,rsn_drg,drug_prn_area,gathering_area,supply_time,consump_time,plc_prchse,sggs_id) 
 values ('$Number_of_Narcotics_Users', '$Type_of_Drug_Used', '$Quantity_of_Drugs_Used','$Price_of_Drugs', '$Reason_for_Drug_Abuse', '$Drug_Abuse_Prone_Area',
 '$Gathering_Area_for_Abuser','$Time_of_Supply','$Time_of_Consumption','$Place_of_Purchase','$Suggestions')";
 
