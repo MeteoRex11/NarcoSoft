4d808ff67cd4906101ae5bc8b4bf19ff5a1dd8bf
JAN151718 <40213316+JAN151718@users.noreply.github.com>
2018-08-05 17:59:11 +0530

    Update Protection_VG.php

--- PHP files/Protection_VG.php
+++ PHP files/Protection_VG.php
@@ -1,18 +1,18 @@
 <?php
 require "Bridge.php";
 
-$Efforts_Made_for_Protection_of_Vulnerable_Group=$_POST["Efforts_Made_for_Protection_of_Vulnerable_Group"];
-$Activities_Promoted_for_Protection_of_Vulnerable_Groups=$_POST["Activities_Promoted_for_Protection_of_Vulnerable_Groups"];
-$Vulnerable_Individuals_Linked_to_a_Positive_Activity=$_POST["Vulnerable_Individuals_Linked_to_a_Positive_Activity"];
-$Individuals_Recommended_for_Specific_Expert_Counselling=$_POST["time"];
-$Problems_Regarding_Acceptance_of_a_Need_of_Protection=$_POST["Problems_Regarding_Acceptance_of_a_Need_of_Protection"];
-$Problems_Regarding_Counselling_Arrangement=$_POST["Problems_Regarding_Counselling_Arrangement"];
-$Other_Problems=$_POST["Other_Problems"];
-$Suggestion=$_POST["Suggestions"];
+$Efforts_Made_for_Protection_of_Vulnerable_Group=$_POST["Effort_for_VG"];
+$Activities_Promoted_for_Protection_of_Vulnerable_Groups=$_POST["Actvty_for_VG"];
+$Vulnerable_Individuals_Linked_to_a_Positive_Activity=$_POST["Vulnrabls_for_actvty"];
+$Individuals_Recommended_for_Specific_Expert_Counselling=$_POST["VG_recommended"];
+$Problems_Regarding_Acceptance_of_a_Need_of_Protection=$_POST["prob_accept_protection"];
+$Problems_Regarding_Counselling_Arrangement=$_POST["counslling_arangment"];
+$Other_Problems=$_POST["probs"];
+$Suggestion=$_POST["suggest"];
 
 
-$mysql_qry = "insert into awareness_info(Efforts_Made_for_Protection_of_Vulnerable_Group, Activities_Promoted_for_Protection_of_Vulnerable_Groups,Vulnerable_Individuals_Linked_to_a_Positive_Activity,Individuals_Recommended_for_Specific_Expert_Counselling,Problems_Regarding_Acceptance_of_a_Need_of_Protection, Problems_Regarding_Counselling_Arrangement,
-Other_Problems,Suggestions) 
+$mysql_qry = "insert into awareness_info(Effort_for_VG, Actvty_for_VG,Vulnrabls_for_actvty,VG_recommended,prob_accept_protection, counslling_arangment,
+probs,suggest) 
 values ('$Efforts_Made_for_Protection_of_Vulnerable_Group', '$Activities_Promoted_for_Protection_of_Vulnerable_Groups', '$Vulnerable_Individuals_Linked_to_a_Positive_Activity','$Individuals_Recommended_for_Specific_Expert_Counselling', '$Problems_Regarding_Acceptance_of_a_Need_of_Protection', '$Problems_Regarding_Counselling_Arrangement','$Other_Problems','$Suggestions')";
 
 
