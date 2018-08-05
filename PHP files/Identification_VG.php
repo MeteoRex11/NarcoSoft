eacd9cc9c82ba747eca734cc5d4ace7b5a3d74bc
JAN151718 <40213316+JAN151718@users.noreply.github.com>
2018-08-05 17:55:16 +0530

    Update Identification_VG.php

--- PHP files/Identification_VG.php
+++ PHP files/Identification_VG.php
@@ -1,13 +1,13 @@
 <?php
 require "Bridge.php";
-$Number_of_Vulnerable_Groups_Identified=$_POST["Number_of_Vulnerable_Groups_Identified"];
-$Number_of_Vulnerable_Groups_Counseled=$_POST["Number_of_Vulnerable_Groups_Counseled"];
-$Problems=$_POST["Problems"];
-$Suggestions=$_POST["Suggestions"];
+$Number_of_Vulnerable_Groups_Identified=$_POST["N_of_VG"];
+$Number_of_Vulnerable_Groups_Counseled=$_POST["N_of_VG_cnsld"];
+$Problems=$_POST["Prob"];
+$Suggestions=$_POST["Suggest"];
 
 
-$mysql_qry = "insert into awareness_info(Number_of_Vulnerable_Groups_Identified, Number_of_Vulnerable_Groups_Counseled, Problems,
-Suggestions) values ('$Number_of_Vulnerable_Groups_Identified', '$Number_of_Vulnerable_Groups_Counseled', '$Problems',
+$mysql_qry = "insert into awareness_info(N_of_VG, N_of_VG_cnsld, Prob,
+Suggest) values ('$Number_of_Vulnerable_Groups_Identified', '$Number_of_Vulnerable_Groups_Counseled', '$Problems',
 '$Suggestions')";
 
 
