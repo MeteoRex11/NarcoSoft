6264fab6248e825706f1d7886c918ba6a17e2b69
JAN151718 <40213316+JAN151718@users.noreply.github.com>
2018-08-05 18:25:20 +0530

    Update deaddiction_motivation.php

--- PHP files/deaddiction_motivation.php
+++ PHP files/deaddiction_motivation.php
@@ -1,18 +1,18 @@
 <?php
 require "Bridge.php";
 
-$Place_of_Approach=$_POST["Place_of_Approach"];
-$Method_of_Approach=$_POST["Method_of_Approach"];
-$Problems_Regarding_Victims=$_POST["Problems_Regarding_Victims"];
-$Problems_Regarding_Victims_Families=$_POST["Problems_Regarding_Victim's_Families"];
-$Arguments_given_by_them=$_POST["Arguments_given_by_them"];
-$Convincing_Factor_Given=$_POST["Convincing_Factor_Given"];
-$Attitude_of_Victims=$_POST["Attitude_of_Victims"];
-$Suggestions=$_POST["Suggestionss"];
+$Place_of_Approach=$_POST["apprch_plc"];
+$Method_of_Approach=$_POST["apprch_mthd"];
+$Problems_Regarding_Victims=$_POST["prblm_victim"];
+$Problems_Regarding_Victims_Families=$_POST["prblm_victim_fam"];
+$Arguments_given_by_them=$_POST["args_victim"];
+$Convincing_Factor_Given=$_POST["cnvnce_factor"];
+$Attitude_of_Victims=$_POST["attd_victim"];
+$Suggestions=$_POST["sggs_mtv"];
 
 
-$mysql_qry = "insert into awareness_info(Place_of_Approach, Method_of_Approach,Problems_Regarding_Victims,Problems_Regarding_Victim's_Families,Arguments_given_by_them, Convincing_Factor_Given,
-Attitude_of_Victims,Suggestionss) 
+$mysql_qry = "insert into awareness_info(apprch_plc,apprch_mthd,prblm_victim,prblm_victim_fam,args_victim, cnvnce_factor,
+attd_victim,sggs_mtv) 
 values ('$Place_of_Approach', '$Method_of_Approach', '$Problems_Regarding_Victims','$Problems_Regarding_Victim's_Families', '$Arguments_given_by_them', '$Convincing_Factor_Given',
 '$Attitude_of_Victims','$Suggestionss')";
 
